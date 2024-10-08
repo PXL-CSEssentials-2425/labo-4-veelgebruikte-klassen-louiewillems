612e214904303e3851a5cbecffc61b4bfbb19bac
louie <11400565@student.pxl.be>
2024-10-08 17:30:59 +0200

    init

--- PasswordMeter/PasswordMeter/MainWindow.xaml.cs
+++ PasswordMeter/PasswordMeter/MainWindow.xaml.cs
@@ -33,6 +33,75 @@ namespace PasswordMeter
         private void passwordMeterButton_Click(object sender, RoutedEventArgs e)
         {
 
+            int passwordStrenght = 4;
+            StringBuilder error = new StringBuilder();
+
+            string password = passwordTextBox.Text.Trim();
+            string userName = userNameTextBox.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(userName) ||
+                string.IsNullOrWhiteSpace(password))
+            {
+                resultTextBlock.Text = "Username or password is empty";
+                return;
+            }
+            else
+            {
+                if (password.Length < 10)
+                {
+                    //error.AppendLine("Password length is smaller then 10 characters");
+                    passwordStrenght--;
+                }
+
+                bool hasUpper = false;
+                bool hasLower = false;
+                bool hasNumber = false;
+
+                foreach (char c in password.ToCharArray())
+                {
+                    if (char.IsNumber(c))
+                        hasNumber = true;
+                    if (char.IsLower(c))
+                        hasLower = true;
+                    if (char.IsUpper(c))
+                        hasUpper = true;
+                }
+
+                if (!hasUpper)
+                {
+                    //error.AppendLine("password has no uppercase");
+                    passwordStrenght--;
+                }
+                if (!hasLower)
+                {
+                    //error.AppendLine("password has no lowercase");
+                    passwordStrenght--;
+                }
+                if (!hasNumber)
+                {
+                    //error.AppendLine("password has no number");
+                    passwordStrenght--;
+                }
+
+                switch (passwordStrenght)
+                {
+                    case 5:
+                        resultTextBlock.Text = "Sterk password";
+                        break;
+                    case 4:
+                        resultTextBlock.Text = "Goed password";
+                        break;
+                    default:
+                        resultTextBlock.Text = "Zwak password";
+                        break;
+                }
+
+                //resultTextBlock.Text = "Succesfull logged in";
+
+            }
+
+
+
         }
     }
 }
